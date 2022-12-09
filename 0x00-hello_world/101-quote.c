ab9e3a3271facc36bec7d739d9e7d6bf4a721970
Elzango <elzango.mzgmail.comgit config --global init.defaultBranch maingit config --global user.email elzango.mzgmail.com>
2022-12-09 01:31:48 +0100

    Fix bugs

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Entry point
@@ -7,8 +6,8 @@
  */
 int main(void)
 {
-	char string1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, string1, 59);
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str, 59);
 	return (1);
 }
-
