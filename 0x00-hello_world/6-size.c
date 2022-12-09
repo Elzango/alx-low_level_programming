6841f165bd777dd5b80543df12fbaaedee99976e
Elzango <elzango.mzgmail.comgit config --global init.defaultBranch maingit config --global user.email elzango.mzgmail.com>
2022-12-09 02:07:37 +0100

    Fix a bug

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,16 +7,17 @@
  */
 int main(void)
 {
-	int a;
-	long int b;
-	long long int c;
-	char d;
+	char a;
+	int b;
+	long int c;
+	long long int d;
 	float f;
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of a long int: %lu byte(s)\n" (unsigned long)sizeof(b));
-	printf("Size of a long long int: %lu byte(s)\n" (unsigned long)sizeof(c));
-	printf("Size of a float: %lu byte(S)\n" (unsigned long)sizeof());
+	printf("Size of a char: %zu bytes\n", sizeof(a));
+	printf("Size of int: %zu bytes\n", sizeof(b));
+	printf("Size of long int: %zu bytes\n", sizeof(c));
+	printf("Size of long long int: %zu bytes\n", sizeof(d));
+	printf("Size of float: %zu bytes\n", sizeof(f));
+
 	return (0);
 }
