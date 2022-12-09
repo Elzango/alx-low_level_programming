5bb966e662e679b6f29d01cec3c4e7b8dd23e4b3
Elzango <elzango.mzgmail.comgit config --global init.defaultBranch maingit config --global user.email elzango.mzgmail.com>
2022-12-09 01:03:28 +0100

    Fix bug

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,8 @@
  */
 int main(void)
 {
-        char string1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-        write(2, string1, 59);
-        return (1);
+	char string1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, string1, 59);
+	return (1);
 }
 
