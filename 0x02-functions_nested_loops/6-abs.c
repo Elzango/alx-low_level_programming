9da7b22ad795f323a0f2945a3ae65b1a2f315da1
Elzango <elzango.mzgmail.comgit config --global init.defaultBranch maingit config --global user.email elzango.mzgmail.com>
2022-12-14 00:25:10 +0100

    Add scripts tothe project

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
- * abs - Computes the absolute value of a given number.
+ *_abs - Computes the absolute value of a given number.
  *
- * Return: Always 0 (Success)
+ * @n: input number as an integer
+ *
+ * Return: absolute value
  */
 int _abs(int n)
 {
 	if (n < 0)
-		return (n*(-1));
+		return (n * (-1));
 	else
 		return (n);
 }
