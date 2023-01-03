407428b4af1ba4d33cf128afdc0208e66bc6aee6
Elzango <elzango.mzgmail.comgit config --global init.defaultBranch maingit config --global user.email elzango.mzgmail.com>
2023-01-03 01:18:56 +0100

    Fix bugs in some of the scripts

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,20 +1,18 @@
 #include "main.h"
-#include <stddef.h>
 /**
- * _strchr - locates a character in a string
- * @s: the given string to be searched
- * @c: the character to find in the given string
- * Return: returns a pointer to first occurrence of the character
- * or a NULL if the character is not found
+ * _strchr - locates a character in a string,
+ * @s: string.
+ * @c: character.
+ * Return: the pointer to the first occurrence of the character c.
  */
 char *_strchr(char *s, char c)
-
 {
-	while (*s != '\0')
-	{
-		if (*s == c)
-			return (s);
-		s++;
-	}
-	return (NULL);
+	unsigned int i = 0;
+
+	for (; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
+			return (s + i);
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
 }
