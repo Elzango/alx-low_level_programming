e8672d9041feeddf3181e7ed6201291f9f2a6650
Elzango <elzango.mzgmail.comgit config --global init.defaultBranch maingit config --global user.email elzango.mzgmail.com>
2023-01-04 20:53:49 +0100

    Fix a bug

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,5 @@
 #include "main.h"
 /**
- * _sqrt_recursion - computes the natural sqrt of a given number
- * @n: the given integer
- * Return: return the sqrt of perfect squares or -1 otherwise
- */
-
-/**
  * power_operation - returns the natural square root of a number.
  * @n: input number.
  * @c: iterator.
@@ -22,6 +16,11 @@ int power_operation(int n, int c)
 	}
 	return (0 + power_operation(n, c + 1));
 }
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: input number.
+ * Return: natural square root.
+ */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
