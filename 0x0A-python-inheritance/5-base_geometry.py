3a89f41379c5a4106adbaea5bf25bc361272de95
Elzango <elzango.mzgmail.comgit config --global init.defaultBranch maingit config --global user.email elzango.mzgmail.com>
2023-04-11 21:57:11 +0100

    Add scripts for the project python inheritence

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+# Arth: Mahmud
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
+Footer
+
